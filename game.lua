78d3a5f8e2283ad7d0ce0129d7ceed32af408e31
AuryArthan <sean.arthan@protonmail.com>
2024-06-16 22:21:19 +0200

    polish

--- game.lua
+++ game.lua
@@ -385,7 +385,7 @@ function Game:pause_update()
 	if CUR_A then
 		A = true
 		if self.PauseHighSq == 1 then self.Paused = not self.Paused end
-		if self.PauseHighSq == 2 then self.Paused = false; Board:init(); Sounds.BackMusic:stop(); Sounds:init(); self.Newgame = true end
+		if self.PauseHighSq == 2 then self.Paused = false; Game:reinit(); Board:init(); Sounds.BackMusic:stop(); Sounds:init(); self.Newgame = true end
 	end
 	
 end
